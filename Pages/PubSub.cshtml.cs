572dda085c15c65ada3f408449cc556635f2508b
GyroGearl00se <bkarttas@gmail.com>
2024-04-16 22:22:48 +0200

    added PubSub page (to be implemented)

---
+++ Pages/PubSub.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Solace_Web_Client.Pages
+{
+    public class PubSubModel : PageModel
+    {
+        private readonly ILogger<PubSubModel> _logger;
+
+        public PubSubModel(ILogger<PubSubModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
